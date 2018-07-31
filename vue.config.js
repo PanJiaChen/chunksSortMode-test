7095008ad586f1dacb8ebbd5fd62ad3d33437764
Pan <panfree23@gmail.com>
2018-07-31 13:10:10 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,11 @@ module.exports = {
   chainWebpack: config => {
     // remove the Preload plugin
     config.plugins.delete('preload')
+
+  },
+  configureWebpack: {
+    optimization: {
+      runtimeChunk: 'single'
+    }
   }
 }
